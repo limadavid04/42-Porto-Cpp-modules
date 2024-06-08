02a416797788f5c25fc88ddd6f77bfdfa6cb8e1e
David <david.04.fsl@gmail.com>
2024-06-08 16:25:41 +0100

    finished ex01

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -1,6 +1,8 @@
 #include "Fixed.hpp"
 #include <iostream>
 #include <cmath>
+#include <climits>
+
 Fixed::Fixed(void) : _fp_value(0) {
 	std::cout << "Default constructor called" << std::endl;
 }
@@ -10,15 +12,15 @@ Fixed::Fixed(const Fixed &cpy) {
 	*this = cpy;
 }
 
-Fixed::Fixed(const int n) : _fp_value(n) {
-	std::cout << "const int constructor called" << std::endl;
+Fixed::Fixed(const int n) {
+	std::cout << "Int constructor called" << std::endl;
+	_fp_value = n << _fp_frac_bits;
 }
 
 Fixed::Fixed(const float n) {
-	std::cout << "const float constructor called" << std::endl;
+	std::cout << "Float constructor called" << std::endl;
 	_fp_value = roundf(n * (1 << _fp_frac_bits));
 }
-
 
 Fixed::~Fixed() {
 	std::cout << "Destructor called" << std::endl;
@@ -26,20 +28,31 @@ Fixed::~Fixed() {
 
 Fixed & Fixed::operator=(const Fixed &src) {
 	if (this != &src)
-	std::cout << "Copy assignment operator called" << std::endl;
 		_fp_value = src._fp_value;
+	std::cout << "Copy assignment operator called" << std::endl;
 	return (*this);
 }
 
 int Fixed::getRawBits(void) const {
-	std::cout << "getRawBits member function called" << std::endl;
 	return (_fp_value);
 }
+
 void Fixed::setRawBits(int const raw)
 {
-	std::cout << "setRawBits member function called" << std::endl;
 	_fp_value = raw;
-
 }
+
+float Fixed::toFloat(void) const {
+	return ((float)_fp_value / (1 << _fp_frac_bits));
+}
+
+int	Fixed::toInt(void) const {
+	return (_fp_value >> _fp_frac_bits);
+}
+
 const int Fixed::_fp_frac_bits = 8;
 
+std::ostream & operator<<(std::ostream &out, const Fixed &fp) {
+	out << fp.toFloat();
+	return (out);
+}
--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -1,18 +1,7 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
-// • A constructor that takes a constant integer as a parameter.
-// It converts it to the corresponding fixed-point value. The fractional bits value is
-// initialized to 8 like in exercise 00.
-// • A constructor that takes a constant floating-point number as a parameter.
-// It converts it to the corresponding fixed-point value. The fractional bits value is
-// initialized to 8 like in exercise 00.
-// • A member function float toFloat( void ) const;
-// that converts the fixed-point value to a floating-point value.
-// • A member function int toInt( void ) const;
-// that converts the fixed-point value to an integer value
-
-
+#include <iostream>
 class Fixed {
 	public:
 		Fixed(void);
@@ -22,11 +11,13 @@ class Fixed {
 		Fixed & operator=(const Fixed &src);
 		~Fixed();
 
+		float toFloat(void) const;
+		int toInt(void) const;
 		int getRawBits(void) const ;
 		void setRawBits(int const raw);
-
 	private:
 		int _fp_value;
 		static const int _fp_frac_bits;
 };
+	std::ostream & operator<<(std::ostream &out, const Fixed &fp);
 #endif
