7c21796dbba1addb995e159635a697c27f513c14
David <david.04.fsl@gmail.com>
2024-06-10 11:36:56 +0100

    added fucntion definitions

--- CPP02/ex02/Fixed.cpp
+++ CPP02/ex02/Fixed.cpp
@@ -32,6 +32,64 @@ Fixed & Fixed::operator=(const Fixed &src) {
 	std::cout << "Copy assignment operator called" << std::endl;
 	return (*this);
 }
+Fixed Fixed::operator+(const Fixed &src) const {
+	return (Fixed(this->_fp_value + src._fp_value));
+}
+Fixed Fixed::operator-(const Fixed &src) const {
+	return (Fixed(this->_fp_value - src._fp_value));
+}
+Fixed Fixed::operator*(const Fixed &src) const {
+	return (Fixed(this->toFloat() * src.toFloat()));
+}
+Fixed Fixed::operator/(const Fixed &src) const {
+	return (Fixed(this->toFloat() / src.toFloat()));
+}
+bool Fixed::operator<(const Fixed &src) const {
+	return (this->toFloat() < src.toFloat());
+}
+bool Fixed::operator<=(const Fixed &src) const {
+	return (this->toFloat() <= src.toFloat());
+}
+bool Fixed::operator>(const Fixed &src) const {
+	return (this->toFloat() > src.toFloat());
+}
+bool Fixed::operator>=(const Fixed &src) const {
+	return (this->toFloat() >= src.toFloat());
+}
+bool Fixed::operator==(const Fixed &src) const {
+	return (this->toFloat() == src.toFloat());
+}
+bool Fixed::operator!=(const Fixed &src) const {
+	return (this->toFloat() != src.toFloat());
+}
+Fixed & Fixed::operator++(void) {
+
+	float fl = this->toFloat();
+	++fl;
+	_fp_value = roundf(fl * (1 << _fp_frac_bits));
+	return (*this);
+}
+Fixed & Fixed::operator--(void){
+	float fl = this->toFloat();
+	--fl;
+	_fp_value = roundf(fl * (1 << _fp_frac_bits));
+	return (*this);
+}
+Fixed Fixed::operator++(int){
+	Fixed tmp = *this;
+	float fl = this->toFloat();
+	fl++;
+	_fp_value = roundf(fl * (1 << _fp_frac_bits));
+	return (tmp);
+}
+
+Fixed Fixed::operator--(int){
+	Fixed tmp = *this;
+	float fl = this->toFloat();
+	fl--;
+	_fp_value = roundf(fl * (1 << _fp_frac_bits));
+	return (tmp);
+}
 
 int Fixed::getRawBits(void) const {
 	return (_fp_value);
@@ -49,7 +107,18 @@ float Fixed::toFloat(void) const {
 int	Fixed::toInt(void) const {
 	return (_fp_value >> _fp_frac_bits);
 }
-
+Fixed & Fixed::min(Fixed &a, Fixed &b) {
+	return (a < b ? a : b);
+}
+Fixed & Fixed::max(Fixed &a, Fixed &b) {
+	return (a > b ? a : b);
+}
+ const Fixed & Fixed::min(const Fixed &a, const Fixed &b) {
+	return (a < b ? a : b);
+}
+const Fixed & Fixed::max(const Fixed &a, const Fixed &b) {
+	return (a > b ? a : b);
+}
 const int Fixed::_fp_frac_bits = 8;
 
 std::ostream & operator<<(std::ostream &out, const Fixed &fp) {
--- CPP02/ex02/Fixed.hpp
+++ CPP02/ex02/Fixed.hpp
@@ -1,6 +1,14 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+// A static member function min that takes as parameters two references on fixed-point
+// numbers, and returns a reference to the smallest one.
+// • A static member function min that takes as parameters two references to constant
+// fixed-point numbers, and returns a reference to the smallest one.
+// • A static member function max that takes as parameters two references on fixed-point
+// numbers, and returns a reference to the greatest one.
+// • A static member function max that takes as parameters two references to constant
+// fixed-point numbers, and returns a reference to the greatest one.
 #include <iostream>
 class Fixed {
 	public:
@@ -9,12 +17,29 @@ class Fixed {
 		Fixed(const int n);
 		Fixed(const float n);
 		Fixed & operator=(const Fixed &src);
+		Fixed operator+(const Fixed &src) const;
+		Fixed operator-(const Fixed &src) const;
+		Fixed operator*(const Fixed &src) const;
+		Fixed operator/(const Fixed &src) const;
+		bool operator<(const Fixed &src) const;
+		bool operator<=(const Fixed &src) const;
+		bool operator>(const Fixed &src) const;
+		bool operator>=(const Fixed &src) const;
+		bool operator==(const Fixed &src) const;
+		bool operator!=(const Fixed &src) const;
+		Fixed & operator++(void); //prefix increment
+		Fixed & operator--(void); //prefix decrement
+		Fixed operator++(int); //postfix increment
+		Fixed operator--(int); //postfix decrement
 		~Fixed();
-
 		float toFloat(void) const;
 		int toInt(void) const;
 		int getRawBits(void) const ;
 		void setRawBits(int const raw);
+		static Fixed & min(Fixed &a, Fixed &b);
+		static Fixed & max(Fixed &a, Fixed &b);
+		static const Fixed & min(const Fixed &a, const Fixed &b);
+		static const Fixed & max(const Fixed &a, const Fixed &b);
 	private:
 		int _fp_value;
 		static const int _fp_frac_bits;
