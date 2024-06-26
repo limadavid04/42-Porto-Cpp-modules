68e4479cf48acbd1583554f3769b3b8857fe0abb
David <david.04.fsl@gmail.com>
2024-06-26 12:43:59 +0100

    finished ex02

---
+++ CPP06/ex02/main.cpp
@@ -0,0 +1,68 @@
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include "Base.hpp"
+#include <iostream>
+#include <stdint.h>
+
+#include <cstdlib>
+#include <ctime>
+
+Base * generate(void) {
+
+	int r = std::rand() % 3;
+	switch (r) {
+		case 0: return (std::cout << "Generated: A" << std::endl, new A());
+		case 1: return (std::cout << "Generated: B" << std::endl, new B());
+		case 2: return (std::cout << "Generated: C" << std::endl,new C());
+	}
+	return NULL;
+}
+
+void identify(Base* p) {
+
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "Not of type A, B or C" << std::endl;
+}
+
+void identify(Base& p) {
+	try {
+		dynamic_cast<A &>(p);
+		std::cout << "A" << std::endl;
+	} catch (std::exception&) {
+		try {
+			dynamic_cast<B &>(p);
+			std::cout << "B" << std::endl;
+		} catch (std::exception&) {
+			try {
+				dynamic_cast<C &>(p);
+				std::cout << "C" << std::endl;
+			} catch (std::exception &)
+			{
+				std::cout << "Not of type A, B or C" << std::endl;
+			}
+		}
+	};
+}
+// It prints the actual type of the object pointed to by p: "A", "B" or "C". Using a pointer
+// inside this function is forbidden.
+int main()
+{
+	srand(time(0));
+	Base *ptr = generate();
+	Base *ptr_2 = generate();
+	identify(ptr);
+	identify(*ptr);
+	identify(ptr_2);
+	identify(*ptr_2);
+
+	delete ptr; // Clean up
+	delete ptr_2;
+	return 0;
+}
