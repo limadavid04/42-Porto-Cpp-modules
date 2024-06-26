47bc68cdf7122bbae1c7da9bf142464927980ade
David <david.04.fsl@gmail.com>
2024-06-26 10:30:08 +0100

    fixed output format to match that of the subject

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -4,7 +4,7 @@
 #include <sstream>
 #include <iostream>
 #include <cmath>
-
+#include <iomanip>
 ScalarType detectScalarType(const std::string& str) {
 	// Check if the str is a single character
 	if (str.length() == 1 && isprint(str[0]) && !isdigit(str[0])) {
@@ -24,13 +24,12 @@ ScalarType detectScalarType(const std::string& str) {
 	if (floatStream >> floatValue) {
 		char c;
 		if (floatStream >> c) { // Attempt to read the next character
-			 if ((c == 'f' || c == 'F') && (floatStream.peek(), floatStream.eof())) {  // 'f' or 'F' is the last character, valid float literal
+			 if ((c == 'f' || c == 'F') && (floatStream.peek(), floatStream.eof())) // 'f' or 'F' is the last character, valid float literal
 				return FLOAT;
 			}
-		else if (str == "-inff" || str == "+inff" || str == "inff" ||str == "nanf")
-			return FLOAT;
-		}
 	}
+	if (str == "-inff" || str == "+inff" || str == "inff" ||str == "nanf")
+		return FLOAT;
 	// Check if the str can be parsed as a double
 	std::istringstream doubleStream(str);
 	double doubleValue;
@@ -42,6 +41,14 @@ ScalarType detectScalarType(const std::string& str) {
 	return UNKNOWN;
 }
 
+const std::string floatSuffix(float value) {
+
+	return ((std::fabs(value - std::floor(value)) < 0.000001) == 1 ?  ".0f" :"f");
+}
+
+const std::string doubleSuffix(double value) {
+	return ((std::fabs(value - std::floor(value)) < 0.000001) == 1 ?  ".0" :"");
+}
 void convertChar(const std::string &str)
 {
 	char charValue = str[0];
@@ -51,8 +58,8 @@ void convertChar(const std::string &str)
 		std::cout << "Char: " << "Non printable" << std::endl;
 	}
 	std::cout << "Int: " << static_cast<int>(charValue) << std::endl;
-	std::cout << "Float: " << static_cast<float>(charValue) << "f" <<std::endl;
-	std::cout << "Double: " << static_cast<double>(charValue) << std::endl;
+	std::cout << "Float: " << static_cast<float>(charValue) << floatSuffix(static_cast<float>(charValue)) <<std::endl;
+	std::cout << "Double: " << static_cast<double>(charValue) << doubleSuffix(static_cast<double>(charValue)) << std::endl;
 }
 void convertInt(const std::string &str)
 {
@@ -64,8 +71,8 @@ void convertInt(const std::string &str)
 		std::cout << "Char: " << "Non printable" << std::endl;
 	}
 	std::cout << "Int: " << intValue << std::endl;
-	std::cout << "Float: " << static_cast<float>(intValue) << "f"<<std::endl;
-	std::cout << "Double: " << static_cast<double>(intValue) << std::endl;
+	std::cout << "Float: " <<static_cast<float>(intValue) << floatSuffix(static_cast<float>(intValue)) <<std::endl;
+	std::cout << "Double: " << static_cast<double>(intValue) << doubleSuffix(static_cast<double>(intValue)) << std::endl;
 }
 void convertFloat(const std::string &str)
 {
@@ -91,8 +98,8 @@ void convertFloat(const std::string &str)
 			}
 			std::cout << "Int: " << static_cast<int>(floatValue) << std::endl;
 		}
-		std::cout << "Float: " << floatValue << "f"<<std::endl;
-		std::cout << "Double: " << static_cast<double>(floatValue) << std::endl;
+		std::cout << "Float: " << floatValue << floatSuffix(floatValue) <<std::endl;
+		std::cout << "Double: " << static_cast<double>(floatValue) << doubleSuffix(static_cast<double>(floatValue)) << std::endl;
 	} else {
 		if (str == "-inff")
 			floatValue = -std::numeric_limits<float>::infinity();
@@ -105,7 +112,7 @@ void convertFloat(const std::string &str)
 		{
 			std::cout << "Char: " << "impossible" << std::endl;
 			std::cout << "Int: " << "impossible"  << std::endl;
-			std::cout << "Float: " << floatValue << "f"<<std::endl;
+			std::cout << "Float: " << floatValue <<"f"<<std::endl;
 			std::cout << "Double: " << static_cast<double>(floatValue) << std::endl;
 		}
 	}
@@ -135,8 +142,8 @@ void convertDouble(const std::string &str)
 			}
 			std::cout << "Int: " << static_cast<int>(doubleValue) << std::endl;
 		}
-		std::cout << "Float: " << static_cast<float>(doubleValue) << "f"<<std::endl;
-		std::cout << "Double: " << doubleValue << std::endl;
+		std::cout << "Float: " << static_cast<float>(doubleValue) << floatSuffix(static_cast<float>(doubleValue)) <<std::endl;
+		std::cout << "Double: " << doubleValue << doubleSuffix(doubleValue) <<std::endl;
 	} else {
 		if (str == "-inf")
 			doubleValue = -std::numeric_limits<double>::infinity();
@@ -148,7 +155,7 @@ void convertDouble(const std::string &str)
 		{
 			std::cout << "Char: " << "impossible" << std::endl;
 			std::cout << "Int: " << "impossible"  << std::endl;
-			std::cout << "Float: " << static_cast<float>(doubleValue) << "f"<<std::endl;
+			std::cout << "Float: " << static_cast<float>(doubleValue) << "f" <<std::endl;
 			std::cout << "Double: " << doubleValue << std::endl;
 		}
 	}
@@ -156,7 +163,6 @@ void convertDouble(const std::string &str)
 
 void ScalarConverter::convert(const std::string &str) {
 	ScalarType type = detectScalarType(str);
-
 	switch (type) {
 		case CHAR: {
 			convertChar(str);
